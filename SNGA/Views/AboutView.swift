832a2bc3d83e86ce5e347dea17a3b3bb9202ef83
gongsichao <gongsc@live.cn>
2026-08-06 09:54:06 +0800

    Bump version to 1.7.1

--- SNGA/Views/AboutView.swift
+++ SNGA/Views/AboutView.swift
@@ -10,7 +10,7 @@ struct AboutView: View {
 
     private let version = Bundle.main.object(
         forInfoDictionaryKey: "CFBundleShortVersionString"
-    ) as? String ?? "1.7.0"
+    ) as? String ?? "1.7.1"
     private let build = Bundle.main.object(
         forInfoDictionaryKey: "CFBundleVersion"
     ) as? String ?? "1"
